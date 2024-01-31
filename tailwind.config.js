625e12c45b202eaff75a567860ef2a3ac9de58ed
Hassan Ali <139656401+hasanbut17314@users.noreply.github.com>
2024-01-31 17:09:53 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html", "./dist/about.html", "./dist/enroll.html", "./dist/contact.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
